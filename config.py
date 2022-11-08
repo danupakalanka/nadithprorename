7f1abefb12aee68c8409e8834bc9196da6d194fe
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-08 20:27:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = "14622685"
+
+API_HASH = "0a0cf89761b30b0af6b46fc4dd845e06"
+
+BOT_TOKEN = "5592599646:AAFUIuyQebqmX241NEHDujtTxUW28T5o7iA"
+
+FORCE_SUB = "tvzonelk"
+
+DB_NAME = "Cluster0"
+
+DB_URL = "mongodb+srv://nadithpro:robo123@cluster0.lnpouoi.mongodb.net/?retryWrites=true&w=majority"
+ 
+FLOOD = int(os.environ.get("FLOOD", "10"))
+
+START_PIC = "https://telegra.ph/file/5f2c2213e479a958564bd.jpg"
+
+ADMIN = "5781386540"
